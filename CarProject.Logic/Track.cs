16ed76c0f6cb69bb9c46eba923bb61f0bc35af2e
ad220296 <145586843+ad220296@users.noreply.github.com>
2024-12-03 19:03:04 +0100

    added Tracktest

---
+++ CarProject.Logic/Track.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarProject.Logic
+{
+    public class Track
+    {
+        private List<Section> _trackList;
+        
+        public Track(List<Section> trackList)
+        {
+            this._trackList = trackList;
+        }
+
+        public Section? StartSection { get => _trackList[0]; }
+    }
+}
---
+++ CarProject.UnitTests/TrackTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarProject.Logic;
+
+namespace CarProject.UnitTests
+{
+    [TestClass]
+    public class TrackTests
+    {
+        [TestMethod]
+        public void ItShouldCreateATrack_GivenAnyNumbersOfSections()
+        {
+            List<Section> trackList = new();
+            Section startSection = new Section(50, 300);
+            Section section = new Section(70, 500);
+            Section lastSection = new Section(60, 200);
+            trackList.Add(startSection);
+            trackList.Add(section);
+            trackList.Add(lastSection);
+
+            Track track = new Track(trackList);
+
+            Assert.AreEqual(startSection, track.StartSection);
+        }
+    }
+}
