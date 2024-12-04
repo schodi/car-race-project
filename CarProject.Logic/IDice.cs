af90a12da6c8bf14fee0c9d1c36a7be3666c3982
Enki <cell.enohp.apps@gmail.com>
2024-12-04 08:32:21 +0100

    restructured IDice Interface, for better readability

--- CarProject.Logic/IDice.cs
+++ CarProject.Logic/IDice.cs
@@ -1,20 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace CarProject.Logic;
 
-namespace CarProject.Logic
+// Interface für einen Würfel (Dice) mit den grundlegenden Methoden und Eigenschaften.
+public interface IDice
 {
-    // Interface für einen Würfel (Dice) mit den grundlegenden Methoden und Eigenschaften.
-    public interface IDice
-    {
-        #region properties
-        public int Dots { get; }
-        #endregion properties
+  #region properties
+  public int Dots { get; }
+  #endregion
 
-        #region methods
-        public void Roll();
-        #endregion methods
-    }
+  #region public methods
+  public void Roll();
+  #endregion
+
 }
