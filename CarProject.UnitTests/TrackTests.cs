0ded76fbc61c0abf49c2c01ba430fa991ece2dc0
Enki <cell.enohp.apps@gmail.com>
2024-12-04 08:26:54 +0100

    restructured TrackTest for better readability

--- CarProject.UnitTests/TrackTests.cs
+++ CarProject.UnitTests/TrackTests.cs
@@ -9,9 +9,10 @@ public class TrackTests
   [TestMethod]
   public void ItShouldCreateATrack_GivenAnyNumbersOfSections()
   {
-    Section startSection = new(50 , 300);
-    Section section = new(70 , 500);
-    Section lastSection = new(60 , 200);
+    Section 
+      startSection = new(50 , 300),
+      section = new(70 , 500),
+      lastSection = new(60 , 200);
 
     List<Section> trackList = [ startSection , section , lastSection ];
 
