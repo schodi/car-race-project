1010d7132c614d5a07420497cea44bc87bc21fd1
Enki <cell.enohp.apps@gmail.com>
2024-12-04 08:24:19 +0100

    restructured some tests, removed an unused test from Tracktest, removed unused usings

--- CarProject.UnitTests/SectionTest.cs
+++ CarProject.UnitTests/SectionTest.cs
@@ -1,67 +1,70 @@
 ﻿using CarProject.Logic;
 
-namespace CarProject.UnitTests
+namespace CarProject.UnitTests;
+
+[TestClass]
+public class SectionTest
 {
-    [TestClass]
-    public class SectionTest
-    {
-        [TestMethod]
-        public void ItShouldHaveALengthAndAMaxSpeed_GivenObjectCreated()
-        {
-            // ARRANGE - Setzen der Testdaten für MaxSpeed und Länge
-            var someSpeed = 60;
-            var someLength = 400;
 
-            // ACT - Erstellen eines neuen Section-Objekts mit den Testdaten
-            Section section = new Section(someSpeed, someLength);
+  [TestMethod]
+  public void ItShouldHaveALengthAndAMaxSpeed_GivenObjectCreated()
+  {
+    // ARRANGE - Setzen der Testdaten für MaxSpeed und Länge
+    var someSpeed = 60;
+    var someLength = 400;
 
-            // ASSERT - Überprüfen, ob MaxSpeed und Länge korrekt gesetzt wurden
-            Assert.AreEqual(someSpeed, section.MaxSpeed); // Erwartung: MaxSpeed ist 60.
-            Assert.AreEqual(someLength, section.Length); // Erwartung: Länge ist 400.
-        }
-        [TestMethod]
-        public void ItShouldConnectASectionAfterTheCurrentSection_GivenAddAfterMeIsCalled()
-        {
-            Section section = new Section(60, 400);
-            Section nextSection = new Section(60, 400);
-            section.AddAfterMe(nextSection);
+    // ACT - Erstellen eines neuen Section-Objekts mit den Testdaten
+    Section section = new Section(someSpeed , someLength);
 
-            Assert.AreEqual(nextSection, section.NextSection);
-            Assert.AreEqual(section, nextSection.PreviousSection);
-        }
-        [TestMethod]
-        public void ItShouldConnectASectionBeforeTheCurrentSection_GivenAddBeforeMeIsCalled()
-        {
-            Section section = new Section(60, 400);
-            Section previousSection = new Section(60, 400);
-            section.AddBeforeMe(previousSection);
+    // ASSERT - Überprüfen, ob MaxSpeed und Länge korrekt gesetzt wurden
+    Assert.AreEqual(someSpeed , section.MaxSpeed); // Erwartung: MaxSpeed ist 60.
+    Assert.AreEqual(someLength , section.Length); // Erwartung: Länge ist 400.
+  }
 
-            Assert.AreEqual(previousSection, section.PreviousSection);
-        }
-        [TestMethod]
-        public void ItShouldInsertASectionBetweenTwoSections_GivenTwoConnectedSectionsAndAddAfterMeIsCalled()
-        {
-            Section sectionOne = new Section(60, 400);
-            Section sectionTwo = new Section(60, 500);
-            Section insertSection = new Section(50, 300);
+  [TestMethod]
+  public void ItShouldConnectASectionAfterTheCurrentSection_GivenAddAfterMeIsCalled()
+  {
+    Section section = new Section(60 , 400);
+    Section nextSection = new Section(60 , 400);
+    section.AddAfterMe(nextSection);
 
-            sectionOne.AddAfterMe(sectionTwo);
-            sectionOne.AddAfterMe(insertSection);
+    Assert.AreEqual(nextSection , section.NextSection);
+    Assert.AreEqual(section , nextSection.PreviousSection);
+  }
 
-            Assert.AreEqual(sectionTwo, sectionOne.NextSection!.NextSection);
-        }
+  [TestMethod]
+  public void ItShouldConnectASectionBeforeTheCurrentSection_GivenAddBeforeMeIsCalled()
+  {
+    Section section = new Section(60 , 400);
+    Section previousSection = new Section(60 , 400);
+    section.AddBeforeMe(previousSection);
 
-        [TestMethod]
-        public void ItShouldInsertASectionBetweenTwoSections_GivenTwoConnectedSectionsAndAddbeforeMeIsCalled()
-        {
-            Section sectionOne = new Section(60, 400);
-            Section sectionTwo = new Section(60, 500);
-            Section insertSection = new Section(50, 300);
+    Assert.AreEqual(previousSection , section.PreviousSection);
+  }
 
-            sectionOne.AddAfterMe(sectionTwo);
-            sectionTwo.AddBeforeMe(insertSection);
+  [TestMethod]
+  public void ItShouldInsertASectionBetweenTwoSections_GivenTwoConnectedSectionsAndAddAfterMeIsCalled()
+  {
+    Section sectionOne = new Section(60 , 400);
+    Section sectionTwo = new Section(60 , 500);
+    Section insertSection = new Section(50 , 300);
 
-            Assert.AreEqual(sectionTwo, sectionOne.NextSection!.NextSection);
-        }
-    }
+    sectionOne.AddAfterMe(sectionTwo);
+    sectionOne.AddAfterMe(insertSection);
+
+    Assert.AreEqual(sectionTwo , sectionOne.NextSection!.NextSection);
+  }
+
+  [TestMethod]
+  public void ItShouldInsertASectionBetweenTwoSections_GivenTwoConnectedSectionsAndAddbeforeMeIsCalled()
+  {
+    Section sectionOne = new Section(60 , 400);
+    Section sectionTwo = new Section(60 , 500);
+    Section insertSection = new Section(50 , 300);
+
+    sectionOne.AddAfterMe(sectionTwo);
+    sectionTwo.AddBeforeMe(insertSection);
+
+    Assert.AreEqual(sectionTwo , sectionOne.NextSection!.NextSection);
+  }
 }
