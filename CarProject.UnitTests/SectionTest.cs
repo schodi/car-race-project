fa2f4c6c8ecc74ea3a91321fb981932ffeebe88e
Enki <cell.enohp.apps@gmail.com>
2024-12-04 12:52:46 +0100

    added screenshots of already done tests

--- CarProject.UnitTests/SectionTest.cs
+++ CarProject.UnitTests/SectionTest.cs
@@ -14,18 +14,20 @@ public class SectionTest
     var someLength = 400;
 
     // ACT - Erstellen eines neuen Section-Objekts mit den Testdaten
-    Section section = new Section(someSpeed , someLength);
+    Section section = new(someSpeed , someLength);
 
     // ASSERT - Überprüfen, ob MaxSpeed und Länge korrekt gesetzt wurden
-    Assert.AreEqual(someSpeed , section.MaxSpeed); // Erwartung: MaxSpeed ist 60.
-    Assert.AreEqual(someLength , section.Length); // Erwartung: Länge ist 400.
+    Assert.AreEqual(someSpeed , section.MaxSpeed);  // Erwartung: MaxSpeed ist 60.
+    Assert.AreEqual(someLength , section.Length);   // Erwartung: Länge ist 400.
   }
 
   [TestMethod]
   public void ItShouldConnectASectionAfterTheCurrentSection_GivenAddAfterMeIsCalled()
   {
-    Section section = new Section(60 , 400);
-    Section nextSection = new Section(60 , 400);
+    Section
+      section = new(60 , 400),
+      nextSection = new(60 , 400);
+
     section.AddAfterMe(nextSection);
 
     Assert.AreEqual(nextSection , section.NextSection);
@@ -35,8 +37,10 @@ public class SectionTest
   [TestMethod]
   public void ItShouldConnectASectionBeforeTheCurrentSection_GivenAddBeforeMeIsCalled()
   {
-    Section section = new Section(60 , 400);
-    Section previousSection = new Section(60 , 400);
+    Section
+      section = new(60 , 400),
+      previousSection = new(60 , 400);
+
     section.AddBeforeMe(previousSection);
 
     Assert.AreEqual(previousSection , section.PreviousSection);
@@ -45,9 +49,10 @@ public class SectionTest
   [TestMethod]
   public void ItShouldInsertASectionBetweenTwoSections_GivenTwoConnectedSectionsAndAddAfterMeIsCalled()
   {
-    Section sectionOne = new Section(60 , 400);
-    Section sectionTwo = new Section(60 , 500);
-    Section insertSection = new Section(50 , 300);
+    Section
+      sectionOne = new(60 , 400),
+      sectionTwo = new(60 , 500),
+      insertSection = new(50 , 300);
 
     sectionOne.AddAfterMe(sectionTwo);
     sectionOne.AddAfterMe(insertSection);
@@ -58,9 +63,10 @@ public class SectionTest
   [TestMethod]
   public void ItShouldInsertASectionBetweenTwoSections_GivenTwoConnectedSectionsAndAddbeforeMeIsCalled()
   {
-    Section sectionOne = new Section(60 , 400);
-    Section sectionTwo = new Section(60 , 500);
-    Section insertSection = new Section(50 , 300);
+    Section
+      sectionOne = new(60 , 400),
+      sectionTwo = new(60 , 500),
+      insertSection = new(50 , 300);
 
     sectionOne.AddAfterMe(sectionTwo);
     sectionTwo.AddBeforeMe(insertSection);
